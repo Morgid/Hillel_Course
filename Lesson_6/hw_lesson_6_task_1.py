1cacee416cfdebc4d0a8ee63253dbb5e1338fd72
Andrii Podlinnov <97788550+Morgid@users.noreply.github.com>
2023-11-24 16:07:32 +0200

    Add files via upload

--- Lesson_6/hw_lesson_6_task_1.py
+++ Lesson_6/hw_lesson_6_task_1.py
@@ -13,8 +13,8 @@ print('№7 Перевод з цельсія в фаренгейт')
 
 temperature_c = float(input('Введіть кількість градусів в Цельсіях: '))
 
-def temperature_convert(conversion_farenheit):
+def celsium_to_farenheit_convert(conversion_farenheit: int | float):
     conversion_farenheit = (temperature_c * 9 / 5) + 32
     return conversion_farenheit
 
-print(f'Кількість градусів в Фаренгейтах = {temperature_convert(conversion_farenheit=temperature_c)}')
+print(f'Кількість градусів в Фаренгейтах = {celsium_to_farenheit_convert(conversion_farenheit=temperature_c)}')
