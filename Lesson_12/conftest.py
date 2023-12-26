7e9e745f3cd87268fdf3b7afef24046a674d0c35
Podli <podlinnow@gmail.com>
2023-12-27 00:59:32 +0200

    Homework Lesson 12, test lesson 12

--- Lesson_12/conftest.py
+++ Lesson_12/conftest.py
@@ -9,3 +9,11 @@ def fixture_random(request):
     request.cls.response = response
     request.cls.status_code = status_code
     yield response, status_code
+
+@pytest.fixture(scope="class")
+def fixture_search_query_chuck(request):
+    response = requests.request(method="GET", url=f"https://api.chucknorris.io/jokes/search?query=chuck")
+    status_code = response.status_code
+    request.cls.response = response
+    request.cls.status_code = status_code
+    yield response, status_code
