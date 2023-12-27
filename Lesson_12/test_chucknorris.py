07c80be1c8af837d3471a05e8334ffe2f636ec64
Podli <podlinnow@gmail.com>
2023-12-27 15:42:59 +0200

    Homework Lesson 12, test lesson 12

--- Lesson_12/test_chucknorris.py
+++ Lesson_12/test_chucknorris.py
@@ -4,59 +4,27 @@ import requests
 
 @pytest.mark.usefixtures("fixture_random")
 class TestRandom:
+    # перевірити поля "icon_url" чи він не пусти + чи це картинка,  - 2 теста
+    # перевірити чи є ключ "value"  і перевірити його значення - 2 теста
     def test_check_year(self):
         assert int(self.response.json()["created_at"][:4]) > 1990, "All our jokes were created until 1990"
 
     def test_status_code(self):
         assert self.status_code == 200
 
-    def test_icon_url_not_empty(self):
-        assert self.response.json()["icon_url"] == "https://assets.chucknorris.host/img/avatar/chuck-norris.png"
-
-    def test_icon_url_image(self):
-        assert ".png" in self.response.json()["icon_url"]
-
-    def test_value_key_is_there(self):
-        assert self.response.json()["value"]
-
-    def test_value_key_with_text(self):
-        assert "Chuck Norris" in self.response.json()["value"]
 
 
-@pytest.mark.usefixtures("fixture_search_query_chuck")
-class TestSearchJokeWord:
-    def test_search_status_code(self):
-        assert self.status_code == 200
+def test_category():
+    URL = "https://api.chucknorris.io/jokes/categories"
+    response = requests.request(method="GET", url=URL)
+    print(response.json())
 
-    def test_search_word_chuk(self):
-        assert self.response.json()["total"] == 9667
-        # print(self.response.json())
-
-    def test_search_joke_value(self):
-        assert self.response.json()["result"][550][
-                   "value"] == "In Chuck Norris' world a Grand Slam is simply slamming your face into a large shiny piano."
-
-    def test_search_joke_all_keys(self):
-        print(self.response.json()["result"][100])
-        assert ["categories"], ["created_at"],["icon_url"], ["id"], ["updated_at"], ["url"], ["value"]) in self.response.json()["result"][100]
-
-    def test_search_joke_category(self):
-        assert self.response.json()["result"][1]["categories"] == ['explicit']
-
-    def test_search_joke_len_id(self):
-        assert len(self.response.json()["result"][1]["id"]) == 22
-
-# def test_category():
-#     URL = "https://api.chucknorris.io/jokes/categories"
-#     response = requests.request(method="GET", url=URL)
-#     print(response.json())
-#
-# @pytest.mark.parametrize("category",
-#                          requests.request(method="GET", url="https://api.chucknorris.io/jokes/categories").json())
-# def test_categories(category):
-#     URL = f"https://api.chucknorris.io/jokes/random?category={category}"
-#     response = requests.request(method="GET", url=URL)
-#     assert len(response.json()["id"]) == 22
+@pytest.mark.parametrize("category",
+                         requests.request(method="GET", url="https://api.chucknorris.io/jokes/categories").json())
+def test_categories(category):
+    URL = f"https://api.chucknorris.io/jokes/random?category={category}"
+    response = requests.request(method="GET", url=URL)
+    assert len(response.json()["id"]) == 22
 
 # Зробити окремий клас
 # пошук жарту по конретному слову  https://api.chucknorris.io/jokes/search?query={query}
