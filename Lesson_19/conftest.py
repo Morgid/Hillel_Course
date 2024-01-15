3814a906cb7907851d6c909bdef3389d31dd4970
Podli <podlinnow@gmail.com>
2024-01-15 19:36:22 +0200

    Lessons, homework

--- Lesson_19/conftest.py
+++ Lesson_19/conftest.py
@@ -1,14 +1,8 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-
 
 @pytest.fixture
 def chrome():
-    service = Service(executable_path='C:\\Users\\Podli\\OneDrive\\Рабочий стол\\Hillel\\Hillel_Course_AQA_Podlinnov\\Lesson_19\\chromedriver.exe')
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(executable_path="/Users/pavlokostyshen/Desktop/hillel/Hillel_october_23/lesson_19/chromedriver")
     yield driver
-    # driver.close()  # закриває вкладку
-    driver.quit()  # закриває браузер
+    driver.quit()
--- Lesson_19/tests/test_text_box.py
+++ Lesson_19/tests/test_text_box.py
@@ -1,25 +1,40 @@
-from Hillel_Course_AQA_Podlinnov.Lesson_18.TextBoxPage_my import TextBoxPage
+import pytest
 
+from Hillel_october_23.lesson_19.TextBoxPage import TextBoxPage
 
 class TestTextBox:
 
-    def test_text_box(self, chrome):
-        page = TextBoxPage(chrome)
-        page.open()
-        page.fill_full_name_field("Andrii")
-        page.fill_full_email_field("Andrii@email.com")
-        page.fill_full_current_text_area_field("Hello, this is Ololo!1")
-        page.fill_full_permanent_text_area_field("Andrii, some text permanent")
-        page.scroll_down()
-        page.click_submit()
+    # def test_username_fill_and_check(self, chrome):
+    #     page = TextBoxPage(chrome)
+    #     page.open()
+    #     page.fill_full_name_field("Pavlo")
+    #     page.scroll_down()
+    #     page.click_submit()
+    #     name_field = page.get_result_fullname()
+    #     assert name_field.replace("Name:", "") == "Pavlo"
 
-    def test_chek_fill_full_name(self, chrome):
+    @pytest.mark.parametrize("email", ["i@meta", "wrong email"])
+    def test_email_fill_and_check_negative(self, chrome, email):
         page = TextBoxPage(chrome)
         page.open()
-        page.fill_full_name_field("Andrii")
+        page.fill_email_field(email)
         page.scroll_down()
-        name_field = page.get_result_fullname()
         page.click_submit()
-        assert name_field.replace("Name:") == "Andrii"
-        # assert name_field.split(":")[1] == "Andrii"
+        class_of_field = page.get_email_field_element().get_attribute("class")
+        assert "error" in class_of_field
+
+
+
+    # def test_email_fill_and_check(self, chrome):
+    #     #todo HW
+    #     pass
+    #
+    # def test_curr_addr_fill_and_check(self, chrome):
+    #     # todo HW
+    #     pass
+    #
+    # def test_perm_addr_fill_and_check(self, chrome):
+    #     # todo HW
+    #     pass
+
 
