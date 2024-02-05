96c6620dda3d61e09dfd36de1d7294e8b0628bc0
Podli <podlinnow@gmail.com>
2024-02-05 20:14:46 +0200

    Delete Selenium folder from master

--- Selenium/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture
-def chrome():
-    driver = webdriver.Chrome(executable_path="C:\Works\Other\AQA\Hillel_october_23-main\Selenium\chromedriver.exe")
-    yield driver
-    driver.quit()
--- Selenium/tests/test_text_box.py
+++
@@ -1,2 +0,0 @@
-def test_text_box():
-    pass
