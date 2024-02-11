f690d6252343814f03ee847eb959ac0e5c87f387
Andrii Podlinnov <97788550+Morgid@users.noreply.github.com>
2024-02-11 14:40:13 +0200

    Add files via upload
    HW lesson 19

---
+++ Selenium/tests/test_elements.py
@@ -0,0 +1,35 @@
+import pytest
+
+from selenium.webdriver.remote.webelement import WebElement
+
+from Hillel_Course_AQA_Podlinnov.Selenium.DynamicPropertiesPage import PageDynamicProperties
+from Hillel_Course_AQA_Podlinnov.Selenium.ElementsPage import ElementsPage
+
+
+class TestElementsPage:
+    def test_page(self, chrome):
+        page = ElementsPage(chrome)
+        page.open()
+        elements = page.get_elements_page_categories()
+        assert len(elements) == 33
+
+    @pytest.mark.parametrize('elements_all',
+                             ['Text Box', 'Check Box', 'Radio Button', 'Web Tables', 'Buttons', 'Links',
+                              'Broken Links - Images', 'Upload and Download', 'Dynamic Properties', '', '', '', '', '',
+                              '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
+    def test_page_elements_all(self, chrome, elements_all):
+        page = ElementsPage(chrome)
+        page.open()
+        elements = page.get_elements_page_categories()
+        assert elements_all in elements
+
+    # def test_is_button_enabled(self, chrome):
+    #     page = PageDynamicProperties(chrome)
+    #     page.open()
+    #     button: WebElement = page.disable_enable_button()
+    #     button.click()
+    #
+    # def test_is_button_shown(self, chrome):
+    #     page = PageDynamicProperties(chrome).open()  # короткий запис
+    #     button: WebElement = page.button_invisible_visible()
+    #     button.click()
--- Selenium/tests/test_text_box.py
+++ Selenium/tests/test_text_box.py
@@ -1,3 +1,5 @@
+import pytest
+
 from Hillel_Course_AQA_Podlinnov.Selenium.TextBoxPage import TextBoxPage
 
 
@@ -20,6 +22,16 @@ class TestTextBox:
         page.click_submit()
         name_field = page.get_result_email()
         assert name_field.replace("Email:", "") == "andrii@gmail.com"
+
+    @pytest.mark.parametrize("email", ["i@meta", "wrong email"])
+    def test_email_fill_and_check_negative(self, chrome, email):
+        page = TextBoxPage(chrome)
+        page.open()
+        page.fill_email_field(email)
+        page.scroll_down()
+        page.click_submit()
+        class_of_field = page.get_email_field_element().get_attribute("class")
+        assert "error" in class_of_field
 
     def test_curr_addr_fill_and_check(self, chrome):
         page = TextBoxPage(chrome)
