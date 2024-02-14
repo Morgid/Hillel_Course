8a373f5630f136ff5eba513a542cf677dba4635f
Andrii Podlinnov <97788550+Morgid@users.noreply.github.com>
2024-02-14 02:13:04 +0200

    Add files via upload
    Update

--- Selenium/conftest.py
+++ Selenium/conftest.py
@@ -1,13 +1,38 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 
+
+# from selenium.webdriver.chrome.service import Service - щоб запускати хром з екзекютебл пасом
 
 @pytest.fixture
 def chrome():
-    service = Service(
-        executable_path='C:\AQA\Hillel_Course_AQA_Podlinnov\Selenium\chromedriver.exe')
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+# щоб запускати хром з екзекютебл пасом
+# @pytest.fixture
+# def chrome():
+#     service = Service(
+#         executable_path='C:\AQA\Hillel_Course_AQA_Podlinnov\Selenium\chromedriver.exe')
+#     options = webdriver.ChromeOptions()
+#     driver = webdriver.Chrome(service=service, options=options)
+#     yield driver
+#     driver.quit()
+
+@pytest.fixture(scope="class")
+def firefox(request):
+    driver = webdriver.Firefox()
+    request.cls.driver = driver
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="class")
+def chrome_class(request):
+    driver = webdriver.Chrome()
+    request.cls.driver = driver
     yield driver
     driver.quit()
