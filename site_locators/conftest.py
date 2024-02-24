e8914c6f506a51b1c35da42e27d5ebafb831eda6
Podli <podlinnow@gmail.com>
2024-02-24 03:18:41 +0200

    Conftest

--- site_locators/conftest.py
+++ site_locators/conftest.py
@@ -1,10 +1,11 @@
 import pytest
-
 from selenium import webdriver
 
 
-@pytest.fixture
-def chrome():
-    driver = webdriver.Chrome()
+@pytest.fixture(scope="class")
+def firefox(request):
+    driver = webdriver.Firefox()
+    request.cls.driver = driver
+    driver.implicitly_wait(5)
     yield driver
     driver.quit()
