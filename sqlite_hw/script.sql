b4d225683d63da609de461e8f8a3d872fe191364
Andrii Podlinnov <97788550+Morgid@users.noreply.github.com>
2024-03-22 22:43:02 +0200

    Delete sqlite_hw directory

--- sqlite_hw/script.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Console_price(name, price_UAH, quantity) VALUES ('PS5 PlayStation 5 Slim', 20999, 12);
-INSERT INTO Console_price(name, price_UAH, quantity) VALUES ('Valve Steam Deck 64 GB', 23499, 1 );
-INSERT INTO Console_price(name, price_UAH, quantity) VALUES ('Nintendo Switch OLED', 19999.05, 22);
-INSERT INTO Console_price(name, price_UAH, quantity) VALUES ('Microsoft Xbox Series S 1TB', 17999.50, 0);
-
-
--- sqlite_hw/tests/test_database.py
+++
@@ -1,8 +0,0 @@
-import pprint
-
-def test_full_data(database):
-    cursor = database.cursor()
-    result = cursor.execute("select * from Console_price where good_id >= 60;")
-    pprint.pprint(result.fetchall())
-
-
