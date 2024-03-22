e7916e2481e364663cd7f335d8fd179424eab927
Podli <podlinnow@gmail.com>
2024-03-22 23:03:29 +0200

    HomeWork sqlite, +path conftest to db, +db

--- sqlite_hw/conftest.py
+++ sqlite_hw/conftest.py
@@ -4,6 +4,8 @@ import sqlite3
 
 @pytest.fixture
 def database():
-    connection = sqlite3.connect('Console_price.db', isolation_level=None)
+    connection = sqlite3.connect(
+        r'C:\Users\Podli\OneDrive\Рабочий стол\Hillel\Hillel_Course_AQA_Podlinnov\sqlite_hw\Console_price.db',
+        isolation_level=None)
     yield connection
     connection.close()
